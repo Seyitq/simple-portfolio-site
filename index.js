65eadc050434bf07b04bea8ac7fa3ee77d80f461
Seyit <seyyidahmedtopbas@gmail.com>
2023-05-21 15:08:51 +0300

    ❤️

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const box = document.querySelector('.box');
+
+  box.addEventListener('mouseenter', () => {
+    box.style.boxShadow = 'none';
+  });
+
+  box.addEventListener('mouseleave', () => {
+    box.style.boxShadow = '0px 0px 10px rgba(0, 0, 0, 0.3)';
+  });
+});
